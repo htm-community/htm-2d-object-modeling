d334ff3fd04ac11c66f9694d12e3262c5f7c8a3f
Zbysekz <zbysekzapadlik@seznam.cz>
2019-08-01 06:54:52 -0400

    Agent extended with 4 sensors

--- python/tests/test_agent.py
+++ python/tests/test_agent.py
@@ -2,7 +2,7 @@ import unittest
 
 from htm2d.agent import Agent
 from htm2d.environment import TwoDimensionalEnvironment
-
+from htm2d.agent import Direction
 
 class AgentTests(unittest.TestCase):
     def test_createAgent(self):
@@ -20,8 +20,12 @@ class AgentTests(unittest.TestCase):
         env = TwoDimensionalEnvironment(1, 2)
         env.set_feature(0, 0, "foo")
         env.set_feature(0, 1, "bar")
+        env.set_feature(0, 1, "frr")
+        env.set_feature(0, 1, "bar")
+        TODO
         agent = Agent()
-        agent.set_env(env, 0, 0)
-        self.assertEqual(agent.get_feature(), "foo")
-        agent.move(0, 1)
-        self.assertEqual(agent.get_feature(), "bar")
+        agent.set_env(env, 0, 1)
+        self.assertEqual(agent.get_feature(Direction.UP), "foo")#above him should be foo
+
+        agent.move(0, 0)
+        self.assertEqual(agent.get_feature(Direction.DOWN), "bar")#under him should be bar
