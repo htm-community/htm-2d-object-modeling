91ed5bff3037afa13e305c456f7d7ac398190a94
Zbysekz <zbysekzapadlik@seznam.cz>
2019-08-08 02:11:25 -0400

    Fixed indentation to 2spaces for all python code

--- python/htm2d/environment.py
+++ python/htm2d/environment.py
@@ -2,38 +2,38 @@ import yaml
 
 
 class TwoDimensionalEnvironment:
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
-        self._features = [x[:] for x in [[None] * height] * width]
+  def __init__(self, width, height):
+    self._width = width
+    self._height = height
+    self._features = [x[:] for x in [[None] * height] * width]
 
-    def load_object(self, yaml_text):
-        obj = yaml.safe_load(yaml_text)
+  def load_object(self, yaml_text):
+    obj = yaml.safe_load(yaml_text)
+    
+    if self._width < obj.get('width') or self._height < obj.get('height'):
+      raise RuntimeError("Dimension of object is bigger than environment!")
         
-        if self._width < obj.get('width') or self._height < obj.get('height'):
-            raise RuntimeError("Dimension of object is bigger than environment!")
-            
-        for feature in obj.get("features"):
-            x = feature.get("x")
-            y = feature.get("y")
-            data = feature.get("data")
-            
-            if x >= obj.get('width') or y >= obj.get('height'):
-                raise RuntimeError("Feature in object is outside the environment!"+str([x,y]))
-            
-            self._features[x][y] = data
+    for feature in obj.get("features"):
+      x = feature.get("x")
+      y = feature.get("y")
+      data = feature.get("data")
+      
+      if x >= obj.get('width') or y >= obj.get('height'):
+        raise RuntimeError("Feature in object is outside the environment!"+str([x,y]))
+      
+      self._features[x][y] = data
 
-    def size(self):
-        return self._width * self._height
+  def size(self):
+    return self._width * self._height
 
-    def set_feature(self, x, y, f):
-        if x<0 or y<0 or x>=self._width or y>=self._height:
-            raise RuntimeError('Not possible to set features outside borders!')
-        else:
-            self._features[x][y] = f
+  def set_feature(self, x, y, f):
+    if x<0 or y<0 or x>=self._width or y>=self._height:
+      raise RuntimeError('Not possible to set features outside borders!')
+    else:
+      self._features[x][y] = f
 
-    def get_feature(self, x, y):
-        if x<0 or y<0 or x>=self._width or y>=self._height:#return None for out of border positions
-            return None
-        
-        return self._features[x][y]
+  def get_feature(self, x, y):
+    if x<0 or y<0 or x>=self._width or y>=self._height:#return None for out of border positions
+      return None
+    
+    return self._features[x][y]
--- python/tests/test_agent.py
+++ python/tests/test_agent.py
@@ -5,68 +5,65 @@ from htm2d.environment import TwoDimensionalEnvironment
 from htm2d.agent import Direction
 
 class AgentTests(unittest.TestCase):
-    def test_createAgent(self):
-        agent = Agent()
-        self.assertIsNot(None, agent)
+  def test_createAgent(self):
+    agent = Agent()
+    self.assertIsNot(None, agent)
 
-    def test_setAgentEnvironment(self):
-        env = TwoDimensionalEnvironment(2, 2)
-        env.set_feature(0, 0, "foo")
-        agent = Agent()
-        agent.set_env(env, 0, 1)
-        self.assertEqual(agent.get_feature(Direction.UP), "foo")#above him should be foo
+  def test_setAgentEnvironment(self):
+    env = TwoDimensionalEnvironment(2, 2)
+    env.set_feature(0, 0, "foo")
+    agent = Agent()
+    agent.set_env(env, 0, 1)
+    self.assertEqual(agent.get_feature(Direction.UP), "foo")#above him should be foo
 
-    def test_moveAgent(self):
-        env = TwoDimensionalEnvironment(3, 3)
-        env.set_feature(0, 0, "foo1")
-        env.set_feature(0, 1, "foo2")
-        env.set_feature(2, 1, "foo3")
-        env.set_feature(1, 2, "foo4")
-        
-        agent = Agent()
-        agent.set_env(env, 0, 1)
-        self.assertEqual(agent.get_feature(Direction.UP), "foo1")
-        agent.move(1, 1)
-        self.assertEqual(agent.get_feature(Direction.LEFT), "foo2")
-        self.assertEqual(agent.get_feature(Direction.RIGHT), "foo3")
-        self.assertEqual(agent.get_feature(Direction.DOWN), "foo4")
+  def test_moveAgent(self):
+    env = TwoDimensionalEnvironment(3, 3)
+    env.set_feature(0, 0, "foo1")
+    env.set_feature(0, 1, "foo2")
+    env.set_feature(2, 1, "foo3")
+    env.set_feature(1, 2, "foo4")
+    
+    agent = Agent()
+    agent.set_env(env, 0, 1)
+    self.assertEqual(agent.get_feature(Direction.UP), "foo1")
+    agent.move(1, 1)
+    self.assertEqual(agent.get_feature(Direction.LEFT), "foo2")
+    self.assertEqual(agent.get_feature(Direction.RIGHT), "foo3")
+    self.assertEqual(agent.get_feature(Direction.DOWN), "foo4")
 
-    def test_checkBorders(self):
-        env = TwoDimensionalEnvironment(2, 2)
-        
-        agent = Agent()
-        agent.set_env(env, 0, 0)
-        self.assertEqual(agent.get_feature(Direction.UP), None)
-        self.assertEqual(agent.get_feature(Direction.LEFT), None)
-        
-        agent.move(1, 1)
-        self.assertEqual(agent.get_feature(Direction.DOWN), None)
-        self.assertEqual(agent.get_feature(Direction.RIGHT), None)
-        
-    def test_moveDirAgent(self):#ensure that moveDir() behaves in consistent with move()
-        env = TwoDimensionalEnvironment(3, 3)
-        
-        agent1 = Agent()
-        agent1.set_env(env, 0, 1)
-        
-        agent2 = Agent()
-        agent2.set_env(env, 0, 1)
-        
-        self.assertEqual(agent1.get_position(),agent2.get_position())
-        agent1.move(0,2)
-        self.assertNotEqual(agent1.get_position(),agent2.get_position())
-        agent2.moveDir(Direction.DOWN)
-        self.assertEqual(agent1.get_position(),agent2.get_position())
-        agent1.move(0,1)
-        agent2.moveDir(Direction.UP)
-        self.assertEqual(agent1.get_position(),agent2.get_position())
-        agent1.move(1,1)
-        agent2.moveDir(Direction.RIGHT)
-        self.assertEqual(agent1.get_position(),agent2.get_position())
-        agent1.move(0,1)
-        agent2.moveDir(Direction.LEFT)
-        self.assertEqual(agent1.get_position(),agent2.get_position())
-        
-        
-        
+  def test_checkBorders(self):
+    env = TwoDimensionalEnvironment(2, 2)
+    
+    agent = Agent()
+    agent.set_env(env, 0, 0)
+    self.assertEqual(agent.get_feature(Direction.UP), None)
+    self.assertEqual(agent.get_feature(Direction.LEFT), None)
+    
+    agent.move(1, 1)
+    self.assertEqual(agent.get_feature(Direction.DOWN), None)
+    self.assertEqual(agent.get_feature(Direction.RIGHT), None)
+      
+  def test_moveDirAgent(self):#ensure that moveDir() behaves in consistent with move()
+    env = TwoDimensionalEnvironment(3, 3)
+    
+    agent1 = Agent()
+    agent1.set_env(env, 0, 1)
+    
+    agent2 = Agent()
+    agent2.set_env(env, 0, 1)
+    
+    self.assertEqual(agent1.get_position(),agent2.get_position())
+    agent1.move(0,2)
+    self.assertNotEqual(agent1.get_position(),agent2.get_position())
+    agent2.moveDir(Direction.DOWN)
+    self.assertEqual(agent1.get_position(),agent2.get_position())
+    agent1.move(0,1)
+    agent2.moveDir(Direction.UP)
+    self.assertEqual(agent1.get_position(),agent2.get_position())
+    agent1.move(1,1)
+    agent2.moveDir(Direction.RIGHT)
+    self.assertEqual(agent1.get_position(),agent2.get_position())
+    agent1.move(0,1)
+    agent2.moveDir(Direction.LEFT)
+    self.assertEqual(agent1.get_position(),agent2.get_position())
         
