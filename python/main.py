de80545547ce6029478e58765392813cfee4b3f4
Zbyšek Zapadlík <zbysekzapadlik@seznam.cz>
2019-09-08 17:11:20 +0200

    Revert "Renamed to correct TM args"
    This reverts commit 522d1407f60fec46de0bc71bce6a3e2ca8cc1e4d.

--- python/main.py
+++ python/main.py
@@ -111,7 +111,7 @@ def SystemSetup(parameters, verbose=True):
         predictedSegmentDecrement=0.0,
         maxSegmentsPerCell=tmParams["maxSegmentsPerCell"],
         maxSynapsesPerSegment=tmParams["maxSynapsesPerSegment"],
-        extra=locParams["cellCount"]
+        externalPredictiveInputs=locParams["cellCount"],
     )
     tm_info = Metrics([sensorLayer_tm.numberOfCells()], 999999999)
 
@@ -138,8 +138,8 @@ def SystemCalculate():
     sensorLayer_tm.compute(
         activeColumns=sensorLayer_SDR_columns,
         learn=True,
-        extraActive=externalDistalInput,
-        extraWinners=externalDistalInput
+        externalPredictiveInputsActive=externalDistalInput,
+        externalPredictiveInputsWinners=externalDistalInput,
     )  # we don't have columns in Location Layer
 
     if agent.get_position() != [3, 4]:  # HACK ALERT! Ignore at this pos (after reset)
